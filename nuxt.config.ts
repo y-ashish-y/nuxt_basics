6d82349a39d6b0231418a4c6133f50d0089c9917
Ashish <your.email@example.com>
2025-05-11 12:24:21 +0530

    feat: integrate Tailwind CSS with Nuxt configuration and add main CSS file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,14 @@
 /**
  * Nuxt configuration object
  * @see {@link https://nuxt.com/docs/api/configuration/nuxt-config}
- *
- * @property {string} compatibilityDate - Sets the compatibility date for Nuxt features
- * @property {Object} devtools - Configuration for Nuxt DevTools
- * @property {boolean} devtools.enabled - Enables/disables the DevTools
- * @property {Object} alias - Custom path aliases configuration
- * @property {string} alias["@"] - Maps "@" to the project root directory
- *
+
  * Key configurations:
  * - Sets compatibility date to future-proof the application
  * - Enables development tools for better debugging experience
  * - Establishes root directory alias "@" for easier imports
  */
 import { resolve } from "path";
-
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
@@ -22,4 +16,8 @@ export default defineNuxtConfig({
   alias: {
     "@": resolve(__dirname, "./"),
   },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/main.css"],
 });
