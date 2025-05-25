872ec42c51c11c4bcf3b272d0edb5a61fd043028
Ashish <your.email@example.com>
2025-05-25 22:29:59 +0530

    route params in nuxt

---
+++ app/pages/blog/[id].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const route = useRoute();
+console.log(route.params.id);
+const id = route.params.id;
+</script>
+
+<template>
+  <div>we are inside a blog {{ id }}</div>
+</template>
