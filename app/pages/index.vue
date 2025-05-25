910c616a1706ae28104a62990ea9124c7d6454b3
Ashish <your.email@example.com>
2025-05-25 22:18:25 +0530

    routing with nuxtlink

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,20 @@
 <script lang="ts"></script>
 
 <template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/blog">Blog</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+      <li></li>
+    </ul>
+  </nav>
   <div>Hello world</div>
 </template>
 
