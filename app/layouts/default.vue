f09eb9e2d8fa3a9caba4b9452b65e4a8bd502e94
Ashish <your.email@example.com>
2025-05-25 22:50:00 +0530

    adding layouts

---
+++ app/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/blog">Blog</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <slot />
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,20 +1,6 @@
 <script lang="ts"></script>
 
 <template>
-  <nav>
-    <ul>
-      <li>
-        <NuxtLink to="/">Home</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/blog">Blog</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/about">About</NuxtLink>
-      </li>
-      <li></li>
-    </ul>
-  </nav>
   <div>Hello world</div>
 </template>
 
