751ce5f50cb790ac289c847cba2409a24d67fad9
Ashish <your.email@example.com>
2025-06-01 13:29:38 +0530

    seo and meta tags

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,30 @@
-<script lang="ts"></script>
+<script lang="ts">
+useSeoMeta({
+  title: "Main Page",
+  description: "My Nuxt app homepage",
+  ogTitle: "[og:title]",
+  ogDescription: "[og:description]",
+  ogImage: "[og:image]",
+  ogUrl: "[og:url]",
+  twitterTitle: "[twitter:title]",
+  twitterDescription: "[twitter:description]",
+  twitterImage: "[twitter:image]",
+  twitterCard: "summary",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.png",
+    },
+  ],
+});
+</script>
 
 <template>
   <div>Hello world</div>
